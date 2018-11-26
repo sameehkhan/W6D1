3947ceaa50e6439d13d8ff78867d4f22a8609ff4
sameehkhan <sameehkhan1@gmail.com>
2018-11-26 12:18:27 -0800

    Create some with rest operator

--- Args_exercise/args_ex.js
+++ Args_exercise/args_ex.js
@@ -1,8 +1,24 @@
-// sum function
-function sum(args) {
+// sum function w/o rest
+function sum(...args) {
   let sum = 0;
     args.forEach(function(el){
       sum += el;
     });
   return sum;
+}
+
+function sum3(...args) {
+  return args.reduce((prev, curr) => {
+    return prev + curr;
+  });
+}
+
+//sum function w/ rest
+
+function sum2(arguments) {
+  let sum = 0;
+  for (i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  console.log(sum);
 }
