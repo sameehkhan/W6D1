23533cc52088cea4b1d18d918b563c41dd6c4163
sameehkhan <sameehkhan1@gmail.com>
2018-11-26 14:30:55 -0800

    Curried sum function

--- Args_exercise/args_ex.js
+++ Args_exercise/args_ex.js
@@ -47,5 +47,19 @@ Function.prototype.myBind2 = function() {
   };
 };
 
+//curriedSum
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
 
+sumThree(4, 20, 6); // == 30
+
+// you'll write `Function#curry`!
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+f1 = f1(4); // [Function]
+f1 = f1(20); // [Function]
+f1 = f1(6); // = 30
+
+// or more briefly:
+sumThree.curry(3)(4)(20)(6); // == 30
 
