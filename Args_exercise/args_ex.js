eb5edc0f642f320e5c2da98e83c9eab15bf02830
sameehkhan <sameehkhan1@gmail.com>
2018-11-26 14:29:09 -0800

    Create my bind using call and apply

--- Args_exercise/args_ex.js
+++ Args_exercise/args_ex.js
@@ -1,11 +1,11 @@
 // sum function w/o rest
-function sum(...args) {
-  let sum = 0;
-    args.forEach(function(el){
-      sum += el;
-    });
-  return sum;
-}
+// function sum(...args) {
+//   let sum = 0;
+//     args.forEach(function(el){
+//       sum += el;
+//     });
+//   return sum;
+// }
 
 function sum3(...args) {
   return args.reduce((prev, curr) => {
@@ -15,10 +15,37 @@ function sum3(...args) {
 
 //sum function w/ rest
 
-function sum2(arguments) {
+function sum2() {
   let sum = 0;
   for (i = 0; i < arguments.length; i++) {
     sum += arguments[i];
   }
   console.log(sum);
 }
+// using apply
+Function.prototype.myBind = function() {
+  let ctx = arguments[0];
+  let bindArgs = Array.from(arguments).slice(1);
+  let that = this;
+  
+  return function() {
+    let callArgs = Array.from(arguments);
+    return that.apply(ctx, bindArgs.concat(callArgs));
+  };
+};
+
+//using call and rest (...) operator
+// only diff is call instead of apply & ... before bindArgs
+Function.prototype.myBind2 = function() {
+  let ctx = arguments[0];
+  let bindArgs = Array.from(arguments).slice(1);
+  let that = this;
+
+  return function() {
+    let callArgs = Array.from(arguments);
+    return that.call(ctx, ...bindArgs.concat(callArgs));
+  };
+};
+
+
+
