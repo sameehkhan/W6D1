eb58a38d9c1e4e4823e739d3bf8360f71d1ffae3
sameehkhan <sameehkhan1@gmail.com>
2018-11-26 15:20:25 -0800

    Implement curriedSum

--- Args_exercise/args_ex.js
+++ Args_exercise/args_ex.js
@@ -63,3 +63,72 @@ f1 = f1(6); // = 30
 // or more briefly:
 sumThree.curry(3)(4)(20)(6); // == 30
 
+
+// Function.prototype.curry = function(numArgs) {
+//   let allArgs = Array.from(arguments).slice(1);
+//   let num = arguments[0];
+//   let that = this;
+// 
+//   for (i = 0; i < num; i++) {
+//     something = that.apply(allArgs);
+//   }
+// 
+//   return function () {
+//     that.apply(allArgs)
+//   };
+// };
+
+// Function.prototype.curry = function (numArgs) {
+//  const args = [];
+//  const _curriedFn = (arg) => {
+//    args.push(arg);
+//    if (args.length === numArgs) {
+//      // spreading the array into individual arguments
+//      return this(...args); 
+//    } else {
+//      return _curriedFn;
+//    }
+//  };
+//  return _curriedFn;
+// };
+
+
+
+function curriedSum(numArgs) {
+  const numbers = [];
+
+  function _curriedSum(num) {
+    numbers.push(num);
+
+    if (numbers.length === numArgs) {
+      let total = 0;
+
+      numbers.forEach((n) => { total += n; });
+
+      return total;
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum;
+}
+
+
+
+Function.prototype.curry2 = function (numArgs) {
+  let args = [];  //set empty array to store args
+  let curried = (arg) => {  //set function to take care of each argument
+    args.push(arg);  //args, our empty array, will push in each argument if
+    if (args.length === numArgs) {
+      return this(...args);
+    } else {
+      return curried;
+    }
+  };
+  return curried;
+};
+
+
+
+
