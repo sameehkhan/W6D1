dc556d598cbf8c19dc3f347853e754688e219ff4
sameehkhan <sameehkhan1@gmail.com>
2018-11-26 16:34:27 -0800

    Implement surrogate function

--- inheritanceExercise.js
+++ inheritanceExercise.js
@@ -1,7 +1,15 @@
-function MovingObject () {}
+// function MovingObject () {}
+// 
+// function Ship () {}
+// Ship.inherits(MovingObject);
+// 
+// function Asteroid () {}
+// Asteroid.inherits(MovingObject);
 
-function Ship () {}
-Ship.inherits(MovingObject);
 
-function Asteroid () {}
-Asteroid.inherits(MovingObject);
+Function.prototype.inherits = function inherits(Parent) {
+  function Surrogate() {}
+  Surrogate.prototype = Parent.prototype;
+  this.prototype = new Surrogate();
+  this.prototype.constructor = this;
+};
